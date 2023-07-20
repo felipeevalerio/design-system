67a7af2f4039e2be5f30be79b54b1cd077b3b894
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2023-07-20 00:08:55 -0300

    Criando storybook

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Enviar',
+  },
+}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Enviar',
+    size: 'big',
+  },
+}
