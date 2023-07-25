915af2d8e4d422bd6ce1dcee8c9fd2ca70e207c5
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2023-07-24 22:12:17 -0300

    Componentes

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/felipeevalerio.png',
+    alt: 'Felipe Valério',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
