152688874599deba31e231710bfc10169fd40153
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2023-07-26 11:44:23 -0300

    fix: Returnin config from viteFinal

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,10 +18,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
-  viteFinal: (config, { configType}) => {
+  viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/design-system/'
     }
+
+    return config
   }
 };
 export default config;
